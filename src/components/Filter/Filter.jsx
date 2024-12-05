8e4d3654c0049807e3912b41c0bff0978dd230e3
hanif gifar <2210512099@mahasiswa.upnvj.ac.id>
2024-12-05 19:34:52 +0700

    feat:update filter search

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -6,23 +6,23 @@ import { FiDollarSign } from "react-icons/fi";
 const Filter = () => {
   return (
     <div className="bg-white p-4 rounded-md shadow-md w-1/">
-      <h3 className="font-medium text-gray-700">Filter</h3>
+      <h3 className="font-medium text-gray-700 pb-3">Filter</h3>
       <ul className="mt-2 space-y-3">
-        <li className="flex justify-between items-center">
+        <li className="flex justify-between items-center border-b border-gray-600 pb-3">
           <div className="flex items-center space-x-2">
             <FiBox className="w-5 h-5 mr-2" />
             <span>Transit</span>
           </div>
           <button className="text-purple-600">&gt;</button>
         </li>
-        <li className="flex justify-between items-center">
+        <li className="flex justify-between items-center border-b border-gray-600 pb-3">
           <div className="flex items-center space-x-2">
             <FiHeart className="w-5 h-5 mr-2" />
             <span>Fasilitas</span>
           </div>
           <button className="text-purple-600">&gt;</button>
         </li>
-        <li className="flex justify-between items-center">
+        <li className="flex justify-between items-center pb-3">
           <div className="flex items-center space-x-2">
             <FiDollarSign className="w-5 h-5 mr-2" />
             <span>Harga</span>
