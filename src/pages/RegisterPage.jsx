baf1ee37c8759cd7f5d836576e56e8cbf0cf2836
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-12-06 21:57:00 +0700

    feat: fixing layout eye in form input register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -15,13 +15,9 @@ const LoginPage = () => {
 
       <div className="flex w-full lg:w-1/2 flex-col justify-center p-4 lg:p-8">
         <div className="mx-auto w-full max-w-md">
-          <h1 className="text-center text-3xl font-bold text-black sm:text-4xl">
+          <h1 className="text-start px-4 text-3xl font-bold text-black sm:text-4xl">
             Daftar
           </h1>
-          <p className="mx-auto mt-4 text-center text-black">
-            Hello, welcome back. Please enter your details.
-          </p>
-
           <FormRegister />
 
           <p className="mt-6 text-center text-slate-600">
