c0ea6fc512e04ab6b21729c645e761e30c5559dd
hanif gifar <2210512099@mahasiswa.upnvj.ac.id>
2024-12-15 11:40:36 +0700

    feat:fix button to homepage

--- src/components/elements/Button/ButtonHeader.jsx
+++ src/components/elements/Button/ButtonHeader.jsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
 import FilterFlight from "../Filter/Filter";
 
 const ButtonHeader = ({ filter }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row w-full gap-4 md:gap-3">
-      <button className="flex gap-8 bg-[#A06ECE] text-white rounded-[12px] p-5 w-full">
+      <button
+        className="flex gap-8 bg-[#A06ECE] text-white rounded-[12px] p-5 w-full"
+        onClick={() => navigate("/")}
+      >
         <ArrowLeft />
-        <Link to="/">Beranda</Link>
+        Beranda
       </button>
       {filter ? <FilterFlight /> : ""}
     </div>
