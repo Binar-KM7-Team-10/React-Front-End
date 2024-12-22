4599b6a523558e8b5d1b6bed4705551f9882a73d
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-12-23 02:10:21 +0700

    fix: layout cheapest filter price type

--- src/components/fragments/FlightSearch/FlightSearch.jsx
+++ src/components/fragments/FlightSearch/FlightSearch.jsx
@@ -50,10 +50,6 @@ const FlightSearch = () => {
     return dates;
   };
 
-  // useEffect(() => {
-  //   setSelectedDay(dayList[0])
-  // }, [dayList]);
-
   const dates = generateDates(initialDate, days.length);
 
   const [selectedOption, setSelectedOption] = useState("Harga - Termurah");
@@ -136,7 +132,7 @@ const FlightSearch = () => {
         setSelectedDate={setSelectedDate}
         dates={dates}
       />
-      <div className="mx-auto md:mx-36 flex justify-end items-center mt-6">
+      <div className="mx-auto flex justify-end items-center mt-6 max-w-5xl px-12">
         <button
           onClick={() => setIsFilterOpen(true)}
           className="px-4 py-2 rounded-full bg-white border-2 border-[#A06ECE] text-[#7126B5] flex gap-2 text-xs md:text-sm"
@@ -147,8 +143,8 @@ const FlightSearch = () => {
       </div>
 
       {isFilterOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white rounded-lg shadow-lg p-4 md:p-6 w-full md:w-96">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 " >
+          <div className="bg-white rounded-lg shadow-lg p-4 md:p-6 w-full md:w-96 ">
             <div className="flex justify-between items-center pb-4 border-b">
               <span className="text-purple-800 font-bold text-sm md:text-lg">
                 {selectedOption}
