ef97f972201de00827cb238316b82c5c89f11c45
NeoBitose <ramadhanalif430@gmail.com>
2024-12-22 16:39:01 +0700

    fix: route cofiguration

--- src/util/routeConfig.js
+++ src/util/routeConfig.js
@@ -16,20 +16,25 @@ import PrintTicketPage from "../pages/PrintTicket";
 import ForgotPasswordPage from "../pages/ForgotPassword";
 
 const routesConfig = [
+
   { path: "/", element: HomePage, type: "public" },
-  { path: "/reset-password", element: ResetPassword, type: "guest" },
-  { path: "/otp-confirm", element: OtpPage, type: "guest" },
+  { path: "/search", element: SearchPage, type: "public" },
+
   { path: "/login", element: Login, type: "guest" },
   { path: "/register", element: Register, type: "guest" },
-  { path: "/payment/:bookCode", element: Payment, type: "auth" },
-  { path: "/checkout/:id", element: OrderPage, type: "auth" },
-  { path: "/print-ticket/:bookCode", element: PrintTicketPage, type: "public" },
+  { path: "/otp-confirm", element: OtpPage, type: "guest" },
+  { path: "/reset-password", element: ResetPassword, type: "guest" },
   { path: "/forgot-password", element: ForgotPasswordPage, type: "guest" },
-  { path: "/search", element: SearchPage, type: "public" },
+
+  { path: "/checkout/:id", element: OrderPage, type: "auth" },
+  { path: "/payment/:bookCode", element: Payment, type: "auth" },
   { path: "/payment-success/:bookCode", element: PaymentSuccess, type: "auth" },
+  { path: "/print-ticket/:bookCode", element: PrintTicketPage, type: "auth" },
+
   { path: "/history-order", element: HistoryOrder, type: "auth" },
   { path: "/notification", element: NotificationPage, type: "auth" },
   { path: "/profile", element: ProfilePage, type: "auth" },
+  
   { path: "*", element: NotFoundPage, type: null },
 ];
 
